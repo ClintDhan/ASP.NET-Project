0991bd6806c947d15c9390bcf8477efa7418977c
ClintDhan <dhaniellerono@gmail.com>
2024-09-07 20:03:27 +0800

    added bootstrap navbar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ public class HomeController : Controller
     {
         return View();
     }
+     public IActionResult AdminLayout()
+    {
+        return View("~/Views/Home/Admin/AdminLayout.cshtml");
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
